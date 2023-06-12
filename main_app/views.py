93cb713f70d470d3c455f7f3e8e608c999496635
clem-alleynejr <clemjr@hotmail.com>
2023-06-11 23:09:05 -0600

    wrote code for viewing about and all, but server running into error when launching

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 lolchamps = [
-    {'Name': 'Ekko', 'Role': 'Assassin', 'Difficulty': 'High'},
-    {'Name': 'Miss Fortune', 'Role': 'Marksman', 'Difficulty': 'Low'}
+    {'name': 'Ekko', 'role': 'Assassin', 'difficulty': 'High'},
+    {'name': 'Miss Fortune', 'role': 'Marksman', 'difficulty': 'Low'}
 ]
 
+def home(request):
+    return render(request, 'home.html')
+
+def lolchamps_index(request):
+    return render(request, 'lolchamps/index.html', {
+        'lolchamps': lolchamps
+    })
+
+def about(request):
+    return render(request, 'about.html')
