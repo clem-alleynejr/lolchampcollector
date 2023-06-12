cb9fe15d5083d8c22c84b911a1c8e415e2b10c28
clem-alleynejr <clemjr@hotmail.com>
2023-06-11 22:11:25 -0600

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+lolchamps = [
+    {'Name': 'Ekko', 'Role': 'Assassin', 'Difficulty': 'High'},
+    {'Name': 'Miss Fortune', 'Role': 'Marksman', 'Difficulty': 'Low'}
+]
+
