1a623c31961ccd5d867040583e925bd757809930
clem-alleynejr <clemjr@hotmail.com>
2023-06-14 13:57:30 -0600

    At step 9 on part 4 of lab

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import LolChamp
+from .models import LolChamp, Ability
 
 admin.site.register(LolChamp)
+admin.site.register(Ability)
--- main_app/models.py
+++ main_app/models.py
@@ -11,6 +11,17 @@ class LolChamp(models.Model):
 
     def get_absolute_url(self):
         return reverse('details', kwargs={'lolchamp_id': self.id})
+    
+class Ability(models.Model):
+    name = models.CharField('ability name', max_length=100)
+    description = models.CharField(max_length=500)
+
+    lolchamp = models.ForeignKey(LolChamp, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Ability: {self.name} -- Description: {self.description}'
+    
+
                       
 
 
