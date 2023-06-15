e21bc129b51dd505f421e3ab36e62116391c9513
clem-alleynejr <clemjr@hotmail.com>
2023-06-15 02:00:02 -0600

    finished part 4 lab

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('lolchamps/<int:lolchamp_id>/', views.lolchamp_details, name='details'),
     path('lolchamps/create/', views.LolChampCreate.as_view(), name='lolchamps_create'),
     path('lolchamps/<int:pk>/update/', views.LolChampUpdate.as_view(), name='lolchamps_update'),
-    path('lolchamps/<int:pk>/delete/', views.LolChampDelete.as_view(), name='lolchamps_delete')
+    path('lolchamps/<int:pk>/delete/', views.LolChampDelete.as_view(), name='lolchamps_delete'),
+    path('lolchamps/<int:lolchamp_id>/add_ability/', views.add_ability, name='add_ability')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import LolChamp
+from .forms import AbilityForm
 
 def home(request):
     return render(request, 'home.html')
@@ -16,7 +17,11 @@ def about(request):
 
 def lolchamp_details(request, lolchamp_id):
     lolchamp = LolChamp.objects.get(id=lolchamp_id)
-    return render(request, 'lolchamps/details.html', {'lolchamp': lolchamp})
+    ability_form = AbilityForm()
+    return render(request, 'lolchamps/details.html', {
+        'lolchamp': lolchamp,
+        'ability_form': ability_form
+        })
 
 class LolChampCreate(CreateView):
     model = LolChamp
@@ -30,3 +35,11 @@ class LolChampUpdate(UpdateView):
 class LolChampDelete(DeleteView):
     model = LolChamp
     success_url = '/lolchamps'
+
+def add_ability(request, lolchamp_id):
+    form = AbilityForm(request.POST)
+    if form.is_valid():
+        new_ability = form.save(commit=False)
+        new_ability.lolchamp_id = lolchamp_id
+        new_ability.save()
+    return redirect('details', lolchamp_id=lolchamp_id)
